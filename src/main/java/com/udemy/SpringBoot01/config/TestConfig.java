2419f7f557b388153ab2de32a302713f823fa99c
Vinicius-E <viniciuseduardo0702@hotmail.com>
2021-12-06 17:15:35 -0300

    Payment one-to-one association

--- src/main/java/com/udemy/SpringBoot01/config/TestConfig.java
+++ src/main/java/com/udemy/SpringBoot01/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.udemy.SpringBoot01.entities.Category;
 import com.udemy.SpringBoot01.entities.Order;
 import com.udemy.SpringBoot01.entities.OrderItem;
+import com.udemy.SpringBoot01.entities.Payment;
 import com.udemy.SpringBoot01.entities.Product;
 import com.udemy.SpringBoot01.entities.User;
 import com.udemy.SpringBoot01.entities.enums.OrderStatus;
@@ -82,5 +83,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 	
 		orderItemsRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}	
 }
